51691fe58daf0f8867b9509fb4d3ab0ef79e5cfd
LAPTOP-2EENOAPP\USER <yjy7643878@google.com>
2021-08-02 17:11:24 +0900

    add GetChickenMain
    실수로 지운 GetChickenMain.java 복구

--- src/api/GetChickenMain.java
+++ src/api/GetChickenMain.java
@@ -1,28 +1,58 @@
 package api;
 
+import java.awt.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+
 public class GetChickenMain {
 
 	public static void main(String[] args) throws Exception {
+		long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss");
+        String getTime = simpleDateFormat.format(date);
+        System.out.println(getTime);
 		
 		GetChickenBithumbAPI getChickenBitumbAPI = new GetChickenBithumbAPI();
 
 		StringBuffer GetTicker = getChickenBitumbAPI.GetTicker("BTC", "KRW");
+		StringBuffer GetOrderBook = getChickenBitumbAPI.GetOrderBook("BTC", "KRW");
+		StringBuffer GetTransaction_History = getChickenBitumbAPI.GetTransaction_History("BTC", "KRW");
+		StringBuffer GetAssets_Status = getChickenBitumbAPI.GetAssets_Status("BTC", "KRW");
+		StringBuffer GetBTCI = getChickenBitumbAPI.GetBTCI("BTC", "KRW");
+		
+//		System.out.println(GetOrderBook);
+//		System.out.println(GetTransaction_History);
+//		System.out.println(GetAssets_Status);
+//		System.out.println(GetBTCI);
 
 
 		String s = GetTicker.toString(); //json object·Î ¸¸µé¾î¾ßµÊ.
 		JSONParser parser = new JSONParser(); //json object·Î ¸¸µé¾îÁÜ
 		
 		JSONObject obj = (JSONObject) parser.parse(s); //JSON Object·Î ¸¸µé¾îÁÜ
-		JSONObject data = (JSONObject)obj.get("data");
-		String opening_price = (String)data.get("opening_price");
+		JSONObject data = (JSONObject)obj.get("data"); //data ¸¸ ²¨³»¿È
+//		String opening_price = (String)data.get("opening_price");
+
+		JSONArray array = new JSONArray();
+		array.add(data);
 		
-		System.out.println(GetTicker);
-		System.out.println(opening_price);
+		List list = new List();
+		
+//		for(List ar : data) {
+//			ar.add(list);
+//		}
+		
+		System.out.println(array);
+		
+//		System.out.println(GetTicker);
+//		System.out.println(opening_price);
 
 	}
 }
@@ -32,13 +62,6 @@ public class GetChickenMain {
 
 
 
-//StringBuffer GetOrderBook = getChickenBitumbAPI.GetOrderBook("BTC", "KRW");
-//StringBuffer GetTransaction_History = getChickenBitumbAPI.GetTransaction_History("BTC", "KRW");
-//StringBuffer GetAssets_Status = getChickenBitumbAPI.GetAssets_Status("BTC", "KRW");
-//StringBuffer GetBTCI = getChickenBitumbAPI.GetBTCI("BTC", "KRW");
 
 
-//System.out.println(GetOrderBook);
-//System.out.println(GetTransaction_History);
-//System.out.println(GetAssets_Status);
-//System.out.println(GetBTCI);
+
